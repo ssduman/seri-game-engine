7fd7c2a2c37ced9aedaadbc35457e51bf2b7352b
ssduman <sduman1818@gmail.com>
2022-07-28 16:15:12 +0300

    changed window creation process

--- src/Window.h
+++ src/Window.h
@@ -5,19 +5,107 @@
 
 #include <iostream>
 
+struct WindowProperties {
+    const char* windowTitle = nullptr;
+    bool isFullscreen = false;
+    int windowWidth = 1280;
+    int windowHeight = 720;
+};
+
 class Window {
-private:
-    int width = 0;
-    int height = 0;
-    double mouseX = 0, mouseY = 0;
-    GLFWwindow* window;
 public:
-    Window(bool fullscreen, int w = 1280, int h = 720);
-    int getWidth();
-    int getHeight();
-    double getMouseX();
-    double getMouseY();
-    void setCursorPosMiddle();
-    GLFWwindow* getWindow();
+    Window(WindowProperties windowProperties) : _windowProperties(windowProperties) {
+        if (!glfwInit()) {
+            std::cout << "glfwInit error" << std::endl;
+            exit(-1);
+        }
+
+        glfwSetErrorCallback(
+            [](int error, const char* description) {
+                fprintf(stderr, "glfw error: %s\n", description);
+            }
+        );
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        if (_windowProperties.isFullscreen) {
+            int monXPos, monYPos;
+            glfwGetMonitorWorkarea(glfwGetPrimaryMonitor(), &monXPos, &monYPos, &_windowProperties.windowWidth, &_windowProperties.windowHeight);
+            _window = glfwCreateWindow(_windowProperties.windowWidth, _windowProperties.windowHeight, _windowProperties.windowTitle, glfwGetPrimaryMonitor(), NULL);
+        } else {
+            _window = glfwCreateWindow(_windowProperties.windowWidth, _windowProperties.windowHeight, _windowProperties.windowTitle, NULL, NULL);
+        }
+
+        if (!_window) {
+            glfwTerminate();
+            std::cout << "window error" << std::endl;
+            exit(-1);
+        }
+
+        glfwMakeContextCurrent(_window);
+        glfwSwapInterval(1);
+
+        if (glewInit() != GLEW_OK) {
+            std::cout << "glewInit Error" << std::endl;
+            exit(-1);
+        }
+
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        //glEnable(GL_CULL_FACE);
+        //glCullFace(GL_FRONT);
+        //glFrontFace(GL_CW);
+        //glFrontFace(GL_CCW);
+        //glCullFace(GL_BACK);
+
+        //setCursorPosMiddle();
+        //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    }
+
+    inline int getWidth() {
+        return _windowProperties.windowWidth;
+    }
+
+    inline int getHeight() {
+        return _windowProperties.windowHeight;
+    }
+
+    inline double getMouseX() {
+        getMousePosition();
+        return _mouseXPosition;
+    }
+
+    inline double getMouseY() {
+        getMousePosition();
+        return _mouseYPosition;
+    }
+
+    inline void setCursorPosMiddle() {
+        glfwSetCursorPos(_window, (double)_windowProperties.windowWidth / 2.0, (double)_windowProperties.windowHeight / 2.0);
+    }
+
+    inline GLFWwindow* getWindow() {
+        return _window;
+    }
+
+private:
+    inline void getMousePosition() {
+        glfwGetCursorPos(_window, &_mouseXPosition, &_mouseYPosition);
+    }
+
+    double _mouseXPosition = 0;
+    double _mouseYPosition = 0;
+    GLFWwindow* _window = nullptr;
+    WindowProperties _windowProperties;
+    /*
+    int _windowWidth = 0;
+    int _windowHeight = 0;
+    bool _isFullscreen = false;
+    const char* _windowTitle = nullptr;
+    */
+
 };
 
