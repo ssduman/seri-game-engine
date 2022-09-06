8488fc367fc68f046670743a5885ecd63610f3ee
ssduman <52033538+ssduman@users.noreply.github.com>
2022-09-06 18:20:52 +0300

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,11 @@
 #include "RunnerSeri.h"
 #include "RunnerMaze.h"
 
-int main(int argc, char** argv) {
 int main(int argc, char* argv[]) {
     try {
         RunnerSeri{}();
         //RunnerMaze{}();
     } catch (const std::exception& ex) {
-        LOGGER(error, "exception occured: " << std::string(ex.what()));
         LOGGER(error, "exception occured: " << ex.what());
     }
 
