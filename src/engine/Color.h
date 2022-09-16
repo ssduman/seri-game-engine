9a5bff9d2debc68f2f06837bd0e30383e3f8e7b1
ssduman <52033538+ssduman@users.noreply.github.com>
2022-09-16 20:43:20 +0300

    updated color class

--- src/engine/Color.h
+++ src/engine/Color.h
@@ -6,8 +6,35 @@ class Color {
 public:
     Color() = default;
 
-    ~Color() = default;
+    Color(float c) {
+        if (c > 1.0f) {
+            c /= 255.0f;
+        }
+        _color.r = c;
+        _color.g = c;
+        _color.b = c;
+    }
 
-    glm::vec4 _color{ 1.0f, 1.0f, 1.0f, 1.0f };
+    Color(float r, float g, float b) {
+        _color.r = r;
+        _color.g = g;
+        _color.b = b;
+    }
+
+    glm::vec3 getColor() {
+        return _color;
+    }
+
+    glm::vec4 getColorRGBA() {
+        return { _color, _alpha };
+    }
+
+    void setAlpha(float alpha) {
+        _alpha = alpha;
+    }
+
+private:
+    float _alpha = 1.0f;
+    glm::vec3 _color{ 1.0f, 1.0f, 1.0f };
 
 };
