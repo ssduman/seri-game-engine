8e83a929434caf577a6ed3e340cd5b67155c394c
ssduman <52033538+ssduman@users.noreply.github.com>
2022-09-16 19:18:52 +0300

    fixed fractal locations

--- src/mics/Fractal.h
+++ src/mics/Fractal.h
@@ -14,26 +14,30 @@ public:
     Fractal(Camera* camera, Layer& layers) : _camera(camera), _layers(layers) {}
 
     void BarnsleyFern() {
-        std::vector<glm::vec3> pointCoordinates{ { 0.0f, 0.0f, 0.0f } };
+        constexpr float z = 10.0f;
+        constexpr float dy = 2.0f;
+
+        std::vector<glm::vec3> pointCoordinates{ { 0.0f, dy, z } };
         std::vector<glm::vec3> pointColors{ { 0.0f, 0.6f, 0.16f } };
 
         constexpr int iteration = 50000;
         for (int i = 0; i < iteration; i++) {
             const auto x = pointCoordinates[i].x;
-            const auto y = pointCoordinates[i].y;
+            const auto y = pointCoordinates[i].y + dy;
+            
             const auto next = _distribution(_generator);
 
             if (next == 1) {
-                pointCoordinates.emplace_back(x, 0.16f * y, 0.0f);
+                pointCoordinates.emplace_back(x, 0.16f * y, z);
             }
             if (next >= 2 && next <= 86) {
-                pointCoordinates.emplace_back(0.85f * x + 0.04f * y, -0.04f * x + 0.85f * y + 1.6f, 0.0f);
+                pointCoordinates.emplace_back(0.85f * x + 0.04f * y, -0.04f * x + 0.85f * y + 1.6f, z);
             }
             if (next >= 87 && next <= 93) {
-                pointCoordinates.emplace_back(0.2f * x - 0.26f * y, 0.23f * x + 0.22f * y + 1.6f, 0.0f);
+                pointCoordinates.emplace_back(0.2f * x - 0.26f * y, 0.23f * x + 0.22f * y + 1.6f, z);
             }
             if (next >= 94 && next <= 100) {
-                pointCoordinates.emplace_back(-0.15f * x + 0.28f * y, 0.26f * x + 0.24f * y + 0.44f, 0.0f);
+                pointCoordinates.emplace_back(-0.15f * x + 0.28f * y, 0.26f * x + 0.24f * y + 0.44f, z);
             }
 
             pointColors.emplace_back(0.0f, 0.6f, 0.16f);
@@ -54,6 +58,8 @@ public:
     void BarnsleyFernAnimation(float deltaTime) {
         static int i = 0;
         static float dx = 0.0f;
+        constexpr float z = 10.0f;
+        constexpr float dy = 2.0f;
         constexpr int iteration = 50000;
 
         dx += deltaTime;
@@ -62,26 +68,26 @@ public:
             std::vector<glm::vec3> pointColors{};
 
             if (i == 0) {
-                pointCoordinates.emplace_back(0.0f, 0.0f, 0.0f);
+                pointCoordinates.emplace_back(0.0f, dy, z);
                 pointColors.emplace_back(0.0f, 0.6f, 0.16f);
             } else {
                 Entity* prev = _layers.getLayers().front();
                 const auto& prevPointCoordinates = prev->getVertices();
                 const auto x = prevPointCoordinates[0];
-                const auto y = prevPointCoordinates[1];
+                const auto y = prevPointCoordinates[1] + dy;
                 const auto next = _distribution(_generator);
 
                 if (next == 1) {
-                    pointCoordinates.emplace_back(x, 0.16f * y, 0.0f);
+                    pointCoordinates.emplace_back(x, 0.16f * y, z);
                 }
                 if (next >= 2 && next <= 86) {
-                    pointCoordinates.emplace_back(0.85f * x + 0.04f * y, -0.04f * x + 0.85f * y + 1.6f, 0.0f);
+                    pointCoordinates.emplace_back(0.85f * x + 0.04f * y, -0.04f * x + 0.85f * y + 1.6f, z);
                 }
                 if (next >= 87 && next <= 93) {
-                    pointCoordinates.emplace_back(0.2f * x - 0.26f * y, 0.23f * x + 0.22f * y + 1.6f, 0.0f);
+                    pointCoordinates.emplace_back(0.2f * x - 0.26f * y, 0.23f * x + 0.22f * y + 1.6f, z);
                 }
                 if (next >= 94 && next <= 100) {
-                    pointCoordinates.emplace_back(-0.15f * x + 0.28f * y, 0.26f * x + 0.24f * y + 0.44f, 0.0f);
+                    pointCoordinates.emplace_back(-0.15f * x + 0.28f * y, 0.26f * x + 0.24f * y + 0.44f, z);
                 }
 
                 pointColors.emplace_back(0.0f, 0.6f, 0.16f);
@@ -143,7 +149,15 @@ private:
         return { cos(radians), sin(radians), 0.0f };
     }
 
-    void buildFractalTree(std::vector<glm::vec3>& coords, std::vector<glm::vec3>& colors, float branchLen, float minBranchLen, float shortenBy, float dirAngle, float angle) {
+    void buildFractalTree(
+        std::vector<glm::vec3>& coords,
+        std::vector<glm::vec3>& colors,
+        float branchLen,
+        float minBranchLen,
+        float shortenBy,
+        float dirAngle,
+        float angle
+    ) {
         if (branchLen > minBranchLen) {
             const float branchLengthNew = branchLen - shortenBy;
 
