4d40880a7665ab5e63c373593b6dfc372fa73ab5
ssduman <52033538+ssduman@users.noreply.github.com>
2022-09-23 20:21:59 +0300

    made food rectangle

--- src/snake/Food.h
+++ src/snake/Food.h
@@ -3,6 +3,7 @@
 #include "../engine/Point.h"
 #include "../engine/Entity.h"
 #include "../engine/Triangle.h"
+#include "../engine/Rectangle.h"
 
 #include "Camera.h"
 
@@ -30,13 +31,32 @@ public:
     void generateFood() {
         const auto x = _distribution(_generator);
         const auto y = _distribution(_generator);
-        _foodPosition = { x * _step - _step / 2.0f, y * _step - _step / 2.0f };
+        _foodPosition = {
+            { x * _step, y * _step },
+            { x * _step, y * _step + _step },
+            { x * _step + _step, y * _step + _step },
+            { x * _step + _step, y * _step },
+        };
+        Rectangle* rectangle = new Rectangle(_camera);
+        rectangle->initShader("snake-assets/shaders/snake_vs.shader", "snake-assets/shaders/snake_fs.shader");
+        rectangle->initMVP();
+        rectangle->setPositionsVec2(_foodPosition);
+        rectangle->setColor(_foodColor);
+        rectangle->init();
+
+        _food = rectangle;
+    }
+
+    void generatePointFood() {
+        const auto x = _distribution(_generator);
+        const auto y = _distribution(_generator);
+        _foodPosition.emplace_back(x * _step - _step / 2.0f, y * _step - _step / 2.0f);
 
         Point* point = new Point(_camera);
         point->initShader("snake-assets/shaders/snake_vs.shader", "snake-assets/shaders/snake_fs.shader");
         point->initMVP();
         point->setDrawMode(GL_POINTS);
-        point->setPositionsVec2({ _foodPosition });
+        point->setPositionsVec2(_foodPosition);
         point->setColor(_foodColor);
         point->init();
 
@@ -59,10 +79,9 @@ public:
         _food = triangle;
     }
 
-
 private:
     Entity* _food;
-    glm::vec2 _foodPosition;
+    std::vector<glm::vec2> _foodPosition;
     glm::vec4 _foodColor{ 0.6f, 2.0f, 0.4f, 1.0f };
 
     float _step{ 50.f };
