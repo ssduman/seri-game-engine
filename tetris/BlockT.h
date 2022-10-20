71f3ed8ed2480c701f8fdda324cee7cd6c79c2e6
ssduman <52033538+ssduman@users.noreply.github.com>
2022-10-20 22:28:14 +0300

    added initial textures for blocks

--- tetris/BlockT.h
+++ tetris/BlockT.h
@@ -41,7 +41,7 @@ public:
         _block = new Polygon(_camera);
         _block->initShader(vertexShader, fragmentShader, /*readFromFile*/ false);
         _block->setDrawMode(GL_TRIANGLES);
-        //_block->setTexture("tetris-assets/textures/block-" + _blockIdentifier + ".png", _blockPositions);
+        _block->setTexture("tetris-assets/textures/block-" + _blockIdentifier + ".png");
         _block->initMVP();
         _block->setPositions(_blockPositions);
         _block->init();
