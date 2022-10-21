585f047d3e51c11aa259d4c197901a35826eb86a
ssduman <52033538+ssduman@users.noreply.github.com>
2022-10-21 23:14:40 +0300

    updated block collusion control

--- tetris/BlockO.h
+++ tetris/BlockO.h
@@ -52,10 +52,10 @@ private:
         const auto x = _tetrisProperties.totalCols / 2.0f - 1.0f;
         const auto y = _tetrisProperties.totalRows - 2.0f;
 
-        const auto s1 = createSquarePosition(x + 0.0f, y + 0.0f);
-        const auto s2 = createSquarePosition(x + 1.0f, y + 0.0f);
-        const auto s3 = createSquarePosition(x + 0.0f, y - 1.0f);
-        const auto s4 = createSquarePosition(x + 1.0f, y - 1.0f);
+        const auto s1 = createSquarePosition(x + 0.0f, y + 1.0f);
+        const auto s2 = createSquarePosition(x + 1.0f, y + 1.0f);
+        const auto s3 = createSquarePosition(x + 0.0f, y + 0.0f);
+        const auto s4 = createSquarePosition(x + 1.0f, y + 0.0f);
 
         _blockPositions.reserve(4 * 4);
         _blockPositions.insert(_blockPositions.end(), s1.begin(), s1.end());
