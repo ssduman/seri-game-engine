61d8e7dc47eb3df5185e5af67be816056406256d
ssduman <52033538+ssduman@users.noreply.github.com>
2022-11-06 15:28:27 +0300

    changed 3rd party library names

--- editor/GUI.h
+++ editor/GUI.h
@@ -10,8 +10,8 @@
 #include "Factory.h"
 
 #include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 class GUI : public Object {
 public:
