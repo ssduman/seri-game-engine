ddc7f6715a5c7bccce7f70b1439541c4d48967e4
ssduman <52033538+ssduman@users.noreply.github.com>
2022-11-07 21:48:27 +0300

    updated logger manager

--- engine/LogManager.h
+++ engine/LogManager.h
@@ -4,11 +4,53 @@
 
 class LogManager {
 public:
-    LogManager(LoggerProperties loggerProperties) : _loggerProperties(loggerProperties) {}
+    LogManager(LogLevel currLogLevel) : _currLogLevel{ currLogLevel } {}
 
-    ~LogManager() = default;
+    LogManager(LogLevel currLogLevel, std::string currMsg) : _currLogLevel{ currLogLevel }, _currMsg{ currMsg } {}
 
-private:
-    LoggerProperties _loggerProperties;
+    ~LogManager() {
+        if (!_currMsg.empty() && _currLogLevel >= _loggerProperties.level) {
+            std::cout << _currMsg;
+        }
+    }
+
+    void setMinimumLogLevel(LogLevel lvl) {
+        _loggerProperties.level = lvl;
+    }
+
+    friend std::ostream& operator<< (std::ostream& out, const LogManager& mng);
+
+    friend std::ostream& operator<< (std::ostream& out, const char* msg);
+
+    friend std::ostream& operator<< (std::ostream& out, const std::string& msg);
+
+    LogLevel _currLogLevel;
+    std::string _currMsg;
+    static inline LoggerProperties _loggerProperties{ LogLevel::info };
 
 };
+
+inline std::ostream& operator<<(std::ostream& out, const LogManager& mng) {
+    out << mng._currMsg;
+    return out;
+}
+
+inline std::ostream& operator<<(std::ostream& out, const char* msg) {
+    out << msg;
+    return out;
+}
+
+inline std::ostream& operator<<(std::ostream& out, const std::string& msg) {
+    out << msg;
+    return out;
+}
+
+#define SERI_LOGGER(lvl) std::cout << LogManager{ lvl }
+
+void test() {
+    std::cout << LogManager{ LogLevel::info, "test" };
+    SERI_LOGGER(LogLevel::info) << "seri logger" << " test 1";
+    SERI_LOGGER(LogLevel::info) << LogManager{ LogLevel::info };
+    std::cout << LogManager{ LogLevel::info } << LogManager{ LogLevel::info } << "test";
+}
+
