15b9731b659653aea47a4e07ae705b5fa54f5ade
ssduman <52033538+ssduman@users.noreply.github.com>
2022-11-13 12:43:58 +0300

    supported model loading via assimp

--- engine/Mesh.h
+++ engine/Mesh.h
@@ -1,11 +1,127 @@
 #pragma once
 
-class Mesh {
-public:
-    Mesh() = default;
+#include "Entity.h"
+#include "Texture.h"
 
-    ~Mesh() = default;
+#include <glm/glm.hpp>
+
+#include <string>
+#include <memory>
+#include <vector>
+
+class Mesh : public Entity {
+public:
+    Mesh() = delete;
+
+    Mesh(Mesh&& other) noexcept : Entity(std::move(other)) {
+        _positions = other._positions;
+        _colors = other._colors;
+        _textureCoords = other._textureCoords;
+        _normals = other._normals;
+        _textures = std::move(other._textures);
+        _indices = other._indices;
+        _drawElementCount = other._drawElementCount;
+    }
+
+    Mesh(const Mesh& other) = delete;
+
+    Mesh& operator=(Mesh&& other) = delete;
+
+    Mesh& operator=(const Mesh& other) = delete;
+
+    Mesh(ICamera* camera) : Entity(camera) {}
+
+    ~Mesh() override = default;
+
+    void init() override {
+        _drawElementCount = static_cast<unsigned int>(_indices.size());
+
+        auto positionsSize = aux::size(_positions);
+        auto colorsSize = aux::size(_colors);
+        auto textureCoordsSize = aux::size(_textureCoords);
+        auto normalsSize = aux::size(_normals);
+
+        reserveDataBuffer(positionsSize + colorsSize + textureCoordsSize + normalsSize);
+        setSubDataBuffer(aux::Index::position, _positions, 0);
+        setSubDataBuffer(aux::Index::color, _colors, positionsSize);
+        setSubDataBuffer(aux::Index::texture, _textureCoords, positionsSize + colorsSize);
+        setSubDataBuffer(aux::Index::normal, _normals, positionsSize + colorsSize + textureCoordsSize);
+
+        dataBuffer({ aux::Target::ebo, aux::size(_indices), aux::data(_indices) });
+    }
+
+    void render() override {
+        unsigned int diffuseNr = 1;
+        unsigned int specularNr = 1;
+        unsigned int normalNr = 1;
+        unsigned int heightNr = 1;
+        for (unsigned int i = 0; i < _textures.size(); i++) {
+            std::string number;
+            std::string typeName = _textures[i].getTypeName();
+            if (typeName == "texture_diffuse") {
+                number = std::to_string(diffuseNr++);
+            }
+            else if (typeName == "texture_specular") {
+                number = std::to_string(specularNr++);
+            }
+            else if (typeName == "texture_normal") {
+                number = std::to_string(normalNr++);
+            }
+            else if (typeName == "texture_height") {
+                number = std::to_string(heightNr++);
+            }
+
+            glActiveTexture(GL_TEXTURE0 + i);
+            std::string nameInShader = "u_texture"; // typeName + number
+            _shader.setInt(nameInShader, i);
+            _textures[i].bind();
+        }
+
+        glBindVertexArray(_VAO);
+        glDrawElements(GL_TRIANGLES, _drawElementCount, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        glActiveTexture(GL_TEXTURE0);
+    }
+
+    void setShader(Shader& shader) {
+        _shader = shader;
+    }
+
+    void addPositions(std::vector<glm::vec3> positions) {
+        _positions.insert(_positions.end(), positions.begin(), positions.end());
+    }
+
+    void addColors(std::vector<glm::vec4> colors) {
+        _colors.insert(_colors.end(), colors.begin(), colors.end());
+    }
+
+    void addTextureCoords(std::vector<glm::vec2> textureCoords) {
+        _textureCoords.insert(_textureCoords.end(), textureCoords.begin(), textureCoords.end());
+    }
+
+    void addNormals(std::vector<glm::vec3> normals) {
+        _normals.insert(_normals.end(), normals.begin(), normals.end());
+    }
+
+    void addTextures(std::vector<Texture> textures) {
+        for (auto& tex : textures) {
+            _textures.emplace_back(std::move(tex));
+        }
+    }
+
+    void addIndices(std::vector<unsigned int> indices) {
+        _indices.insert(_indices.end(), indices.begin(), indices.end());
+    }
 
 private:
+    std::vector<glm::vec3> _positions;
+    std::vector<glm::vec4> _colors;
+    std::vector<glm::vec2> _textureCoords;
+    std::vector<glm::vec3> _normals;
+    std::vector<Texture> _textures;
+    std::vector<unsigned int> _indices;
+
+    unsigned int _drawElementCount{ 0 };
 
 };
--- engine/Model.h
+++ engine/Model.h
@@ -8,9 +8,10 @@
 #include <assimp/Importer.hpp>
 #include <assimp/postprocess.h>
 
+#include <memory>
 #include <vector>
 
-class Model : Entity {
+class Model : public Entity {
 public:
     Model(ICamera* camera) : Entity(camera) {
         LOGGER(info, "model init succeeded");
@@ -20,9 +21,11 @@ public:
 
     void init() override {}
 
-    void update() override {}
-
-    void render() override {}
+    void render() override {
+        for (auto& mesh : _meshes) {
+            mesh.display();
+        }
+    }
 
     void load(const std::string& modelPath) {
         Assimp::Importer importer;
@@ -33,7 +36,10 @@ public:
             return;
         }
 
+        _modelDirectory = modelPath.substr(0, modelPath.find_last_of("/")) + "/";
+
         LOGGER(info, "read model path '" << modelPath << "' succeeded");
+        LOGGER(info, "model directory: " << _modelDirectory);
         LOGGER(info, "model mNumMeshes: " << scene->mNumMeshes);
         LOGGER(info, "model mNumMaterials: " << scene->mNumMaterials);
         LOGGER(info, "model mNumAnimations: " << scene->mNumAnimations);
@@ -41,16 +47,137 @@ public:
         LOGGER(info, "model mNumLights: " << scene->mNumLights);
         LOGGER(info, "model mNumCameras: " << scene->mNumCameras);
         LOGGER(info, "model mNumSkeletons: " << scene->mNumSkeletons);
+
+        processNode(scene->mRootNode, scene);
     }
 
 private:
     unsigned int flagBuilder() {
-        return aiProcess_CalcTangentSpace |
-            aiProcess_Triangulate |
-            aiProcess_JoinIdenticalVertices |
-            aiProcess_SortByPType;
+        return aiProcess_Triangulate |
+            aiProcess_GenSmoothNormals |
+            aiProcess_FlipUVs |
+            aiProcess_CalcTangentSpace;
+    }
+
+    void processNode(const aiNode* node, const aiScene* scene) {
+        for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+            aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+            processMesh(mesh, scene);
+        }
+        for (unsigned int i = 0; i < node->mNumChildren; i++) {
+            processNode(node->mChildren[i], scene);
+        }
+    }
+
+    void processMesh(const aiMesh* mesh, const aiScene* scene) {
+        Mesh mesh_{ _camera };
+
+        std::vector<glm::vec3> positions;
+        std::vector<glm::vec4> colors;
+        std::vector<glm::vec2> textureCoords;
+        std::vector<glm::vec3> normals;
+        for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
+            glm::vec3 position;
+            glm::vec4 color;
+            glm::vec2 textureCoord;
+            glm::vec3 normal;
+
+            if (mesh->HasPositions()) {
+                convertVector(mesh->mVertices[i], position);
+                positions.emplace_back(std::move(position));
+            }
+            if (mesh->HasVertexColors(0)) {
+                convertVector(mesh->mColors[0][i], color);
+                colors.emplace_back(std::move(color));
+            }
+            if (mesh->HasTextureCoords(0)) {
+                convertVector(mesh->mTextureCoords[0][i], textureCoord);
+                textureCoords.emplace_back(std::move(textureCoord));
+            }
+            if (mesh->HasNormals()) {
+                convertVector(mesh->mNormals[i], normal);
+                normals.emplace_back(std::move(normal));
+            }
+            if (mesh->HasBones()) {
+            }
+            if (mesh->HasFaces()) {
+            }
+            if (mesh->HasTangentsAndBitangents()) {
+            }
+            if (mesh->HasTextureCoordsName(0)) {
+            }
+        }
+        mesh_.addPositions(std::move(positions));
+        mesh_.addColors(std::move(colors));
+        mesh_.addTextureCoords(std::move(textureCoords));
+        mesh_.addNormals(std::move(normals));
+
+        std::vector<unsigned int> indices;
+        for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
+            aiFace face = mesh->mFaces[i];
+            for (unsigned int j = 0; j < face.mNumIndices; j++) {
+                indices.emplace_back(face.mIndices[j]);
+            }
+        }
+        mesh_.addIndices(std::move(indices));
+
+        aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+        std::vector<Texture> normalMaps = std::move(loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal"));
+        std::vector<Texture> heightMaps = std::move(loadMaterialTextures(material, aiTextureType_AMBIENT, "texture_height"));
+        std::vector<Texture> diffuseMaps = std::move(loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse"));
+        std::vector<Texture> specularMaps = std::move(loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular"));
+        mesh_.addTextures(std::move(heightMaps));
+        mesh_.addTextures(std::move(normalMaps));
+        mesh_.addTextures(std::move(diffuseMaps));
+        mesh_.addTextures(std::move(specularMaps));
+
+        mesh_.setShader(_shader);
+        mesh_.initMVP();
+        mesh_.init();
+        _meshes.emplace_back(std::move(mesh_));
+    }
+
+    std::vector<Texture> loadMaterialTextures(const aiMaterial* material, const aiTextureType textureType, const std::string& textureTypeName) {
+        std::vector<Texture> textures;
+
+        for (unsigned int i = 0; i < material->GetTextureCount(textureType); i++) {
+            aiString aiTexturePath;
+            material->GetTexture(textureType, i, &aiTexturePath);
+            std::string texturePath = _modelDirectory + std::string(aiTexturePath.C_Str());
+
+            Texture texture;
+            texture.init(texturePath);
+            texture.setTypeName(textureTypeName);
+            textures.emplace_back(std::move(texture));
+        }
+
+        return textures;
+    }
+
+    void static convertVector(const aiVector2D& aiVec, glm::vec2& glmVec) {
+        glmVec.x = aiVec.x;
+        glmVec.y = aiVec.y;
+    }
+
+    void static convertVector(const aiVector3D& aiVec, glm::vec2& glmVec) {
+        glmVec.x = aiVec.x;
+        glmVec.y = aiVec.y;
+    }
+
+    void static convertVector(const aiVector3D& aiVec, glm::vec3& glmVec) {
+        glmVec.x = aiVec.x;
+        glmVec.y = aiVec.y;
+        glmVec.z = aiVec.z;
+    }
+
+    void static convertVector(const aiColor4D& aiVec, glm::vec4& glmVec) {
+        glmVec.r = aiVec.r;
+        glmVec.g = aiVec.g;
+        glmVec.b = aiVec.b;
+        glmVec.a = aiVec.a;
     }
 
     std::vector<Mesh> _meshes;
+    std::string _modelDirectory;
 
 };
