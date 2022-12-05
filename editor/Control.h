f3c8f43353550a91883a791f43084279766ff725
ssduman <52033538+ssduman@users.noreply.github.com>
2022-12-05 19:20:11 +0300

    refactored engine control

--- editor/Control.h
+++ editor/Control.h
@@ -3,12 +3,15 @@
 #pragma warning(disable: 4244)
 
 #include "../engine/IControl.h"
-#include "../engine/InputHandler.h"
-#include "../engine/WindowManager.h"
+
+#include "Camera.h"
+
+#include <memory>
 
 class Control : public IControl {
 public:
-    Control(WindowManager* windowManager, Camera* camera, State* state) : IControl(windowManager, state), _camera(camera), _inputHandler(camera) {
+    Control(std::shared_ptr<WindowManager> windowManager, std::shared_ptr<State> state, std::shared_ptr<Camera> camera)
+        : IControl(windowManager, state), _camera(camera) {
         glfwSetWindowUserPointer(_windowManager->getWindow(), static_cast<void*>(this));
 
         LOGGER(info, "control init succeeded");
@@ -16,56 +19,6 @@ public:
 
     ~Control() override {
         LOGGER(info, "control delete succeeded");
-    }
-
-    void init() override {
-        glfwSetCharCallback(_windowManager->getWindow(),
-            [](GLFWwindow* window, unsigned int codepoint) {
-                if (auto control = static_cast<Control*>(glfwGetWindowUserPointer(window))) {
-                    control->charCallback(window, codepoint);
-                }
-            }
-        );
-
-        glfwSetMouseButtonCallback(_windowManager->getWindow(),
-            [](GLFWwindow* window, int button, int action, int mods) {
-                if (auto control = static_cast<Control*>(glfwGetWindowUserPointer(window))) {
-                    control->mouseButtonCallback(window, button, action, mods);
-                }
-            }
-        );
-
-        glfwSetScrollCallback(_windowManager->getWindow(),
-            [](GLFWwindow* window, double xoffset, double yoffset) {
-                if (auto control = static_cast<Control*>(glfwGetWindowUserPointer(window))) {
-                    control->scrollCallback(window, xoffset, yoffset);
-                }
-            }
-        );
-
-        glfwSetFramebufferSizeCallback(_windowManager->getWindow(),
-            [](GLFWwindow* window, int width, int height) {
-                if (auto control = static_cast<Control*>(glfwGetWindowUserPointer(window))) {
-                    control->framebufferSizeCallback(window, width, height);
-                }
-            }
-        );
-
-        glfwSetCursorPosCallback(_windowManager->getWindow(),
-            [](GLFWwindow* window, double xpos, double ypos) {
-                if (auto control = static_cast<Control*>(glfwGetWindowUserPointer(window))) {
-                    control->cursorPosCallback(window, xpos, ypos);
-                }
-            }
-        );
-
-        glfwSetKeyCallback(_windowManager->getWindow(),
-            [](GLFWwindow* window, int key, int scancode, int action, int mods) {
-                if (auto control = static_cast<Control*>(glfwGetWindowUserPointer(window))) {
-                    control->keyCallback(window, key, scancode, action, mods);
-                }
-            }
-        );
     }
 
     void charCallback(GLFWwindow* window, unsigned int codepoint) override {
@@ -96,17 +49,15 @@ public:
     }
 
     void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) override {
-        if (ICommand* command = _inputHandler.handleInput(key, scancode, action, mods)) {
-            command->execute();
-        }
-
         if ((key == GLFW_KEY_ENTER) && (action == GLFW_PRESS)) {
             _userInputVector.clear();
-        } else if ((key == GLFW_KEY_BACKSPACE) && (action == GLFW_PRESS)) {
+        }
+        else if ((key == GLFW_KEY_BACKSPACE) && (action == GLFW_PRESS)) {
             if (!_userInputVector.empty()) {
                 _userInputVector.pop_back();
             }
-        } else if ((key == GLFW_KEY_ESCAPE) && (action == GLFW_PRESS)) {
+        }
+        else if ((key == GLFW_KEY_ESCAPE) && (action == GLFW_PRESS)) {
             _windowManager->windowShouldClose();
         }
 
@@ -136,12 +87,7 @@ public:
         }
     }
 
-    void setInputHandler(InputHandler& inputHandler) {
-        _inputHandler = inputHandler;
-    }
-
 private:
-    Camera* _camera;
-    InputHandler _inputHandler;
+    std::shared_ptr<Camera> _camera;
 
 };
