7df6572a20efb07310f7d6af2451a3ee59c60223
ssduman <52033538+ssduman@users.noreply.github.com>
2022-12-06 21:37:59 +0300

    fixed mics control

--- mics/Control.h
+++ mics/Control.h
@@ -3,13 +3,16 @@
 #pragma warning(disable: 4244)
 
 #include "../engine/IControl.h"
-#include "../engine/InputHandler.h"
-#include "../engine/WindowManager.h"
+
+#include "Camera.h"
+
+#include <memory>
 
 class Control : public IControl {
 public:
-    Control(WindowManager* windowManager, Camera* camera, State* state) : IControl(windowManager, state), _camera(camera), _inputHandler(camera) {
-        glfwSetWindowUserPointer(_windowManager->getWindow(), static_cast<void*>(this));
+    Control(std::shared_ptr<WindowManager> windowManager, std::shared_ptr<State> state, std::shared_ptr<Camera> camera)
+        : IControl(windowManager, state), _camera(camera) {
+        _windowManager->setWindowUserPointer(static_cast<void*>(this));
 
         LOGGER(info, "control init succeeded");
     }
@@ -18,76 +21,18 @@ public:
         LOGGER(info, "control delete succeeded");
     }
 
-    void init() override {
-        glfwSetCharCallback(_windowManager->getWindow(),
-            [](GLFWwindow* window, unsigned int codepoint) {
-                if (auto control = static_cast<Control*>(glfwGetWindowUserPointer(window))) {
-                    control->charCallback(window, codepoint);
-                }
-            }
-        );
-
-        glfwSetMouseButtonCallback(_windowManager->getWindow(),
-            [](GLFWwindow* window, int button, int action, int mods) {
-                if (auto control = static_cast<Control*>(glfwGetWindowUserPointer(window))) {
-                    control->mouseButtonCallback(window, button, action, mods);
-                }
-            }
-        );
-
-        glfwSetScrollCallback(_windowManager->getWindow(),
-            [](GLFWwindow* window, double xoffset, double yoffset) {
-                if (auto control = static_cast<Control*>(glfwGetWindowUserPointer(window))) {
-                    control->scrollCallback(window, xoffset, yoffset);
-                }
-            }
-        );
-
-        glfwSetFramebufferSizeCallback(_windowManager->getWindow(),
-            [](GLFWwindow* window, int width, int height) {
-                if (auto control = static_cast<Control*>(glfwGetWindowUserPointer(window))) {
-                    control->framebufferSizeCallback(window, width, height);
-                }
-            }
-        );
-
-        glfwSetCursorPosCallback(_windowManager->getWindow(),
-            [](GLFWwindow* window, double xpos, double ypos) {
-                if (auto control = static_cast<Control*>(glfwGetWindowUserPointer(window))) {
-                    control->cursorPosCallback(window, xpos, ypos);
-                }
-            }
-        );
-
-        glfwSetKeyCallback(_windowManager->getWindow(),
-            [](GLFWwindow* window, int key, int scancode, int action, int mods) {
-                if (auto control = static_cast<Control*>(glfwGetWindowUserPointer(window))) {
-                    control->keyCallback(window, key, scancode, action, mods);
-                }
-            }
-        );
-    }
-
     void charCallback(GLFWwindow* window, unsigned int codepoint) override {
-        auto str = encodeUTF8(codepoint);
-        addUserInput(str);
+        addUserInput(encodeUTF8(codepoint));
     }
 
     void cursorPosCallback(GLFWwindow* window, double xpos, double ypos) override {
-        _mouseCurrentPosX = xpos;
-        _mouseCurrentPosY = ypos;
-
-        float xPos = static_cast<float>(xpos);
-        float yPos = static_cast<float>(ypos);
-        _camera->handleMouse(xPos, yPos);
+        _camera->handleMouse(static_cast<float>(xpos), static_cast<float>(ypos));
     }
 
     void scrollCallback(GLFWwindow* window, double xoffset, double yoffset) override {}
 
     void framebufferSizeCallback(GLFWwindow* window, int width, int height) override {
-        _windowManager->getWidth() = width;
-        _windowManager->getHeight() = height;
-        glViewport(0, 0, width, height);
+        _windowManager->viewport(0, 0, width, height);
     }
 
     void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) override {
@@ -96,10 +41,6 @@ public:
     }
 
     void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) override {
-        if (ICommand* command = _inputHandler.handleInput(key, scancode, action, mods)) {
-            command->execute();
-        }
-
         if ((key == GLFW_KEY_ENTER) && (action == GLFW_PRESS)) {
             _userInputVector.clear();
         }
@@ -138,12 +79,7 @@ public:
         }
     }
 
-    void setInputHandler(InputHandler& inputHandler) {
-        _inputHandler = inputHandler;
-    }
-
 private:
-    Camera* _camera;
-    InputHandler _inputHandler;
+    std::shared_ptr<Camera> _camera;
 
 };
