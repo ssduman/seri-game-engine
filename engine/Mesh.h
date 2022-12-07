4b8366606a09b7aae40d1108b953e0d36600392f
ssduman <52033538+ssduman@users.noreply.github.com>
2022-12-07 18:35:57 +0300

    fixed mesh for model

--- engine/Mesh.h
+++ engine/Mesh.h
@@ -1,26 +1,33 @@
 #pragma once
 
-#include "Entity.h"
+#include "Object.h"
+#include "Shader.h"
 #include "Texture.h"
+#include "Material.h"
+#include "ShaderManager.h"
+#include "AuxiliaryStructs.h"
+#include "OpenGLEngineBackend.h"
 
 #include <glm/glm.hpp>
 
-#include <string>
 #include <memory>
+#include <string>
 #include <vector>
 
-class Mesh : public Entity {
+class Mesh : public Object {
 public:
-    Mesh() = delete;
+    Mesh(Shader& shader) : _shader(shader) {}
 
-    Mesh(Mesh&& other) noexcept : Entity(std::move(other)) {
-        _positions = other._positions;
-        _colors = other._colors;
-        _textureCoords = other._textureCoords;
-        _normals = other._normals;
+    Mesh(Mesh&& other) noexcept : _shader(other._shader) {
+        _positions = std::move(other._positions);
+        _colors = std::move(other._colors);
+        _textureCoords = std::move(other._textureCoords);
+        _normals = std::move(other._normals);
         _textures = std::move(other._textures);
-        _indices = other._indices;
-        _drawElementCount = other._drawElementCount;
+        _indices = std::move(other._indices);
+        _transformation = std::move(other._transformation);
+        _shaderManager = std::move(other._shaderManager);
+        _engineBackend = std::move(other._engineBackend);
     }
 
     Mesh(const Mesh& other) = delete;
@@ -29,14 +36,14 @@ public:
 
     Mesh& operator=(const Mesh& other) = delete;
 
-    Mesh(ICamera* camera) : Entity(camera) {}
-
     ~Mesh() override = default;
 
     void init() override {
-        _drawElementCount = static_cast<unsigned int>(_indices.size());
-        if (_drawElementCount > 0) {
-            dataBuffer({ aux::Target::ebo, aux::size(_indices), aux::data(_indices) });
+        auto drawElementCount = aux::count(_indices);
+        if (drawElementCount > 0) {
+            _engineBackend.setDrawCount(drawElementCount);
+            _engineBackend.setDrawType(aux::DrawType::elements);
+            _engineBackend.setDataBuffer(aux::DataBuffer{ aux::Target::ebo, aux::size(_indices), aux::data(_indices) });
         }
 
         auto positionsSize = aux::size(_positions);
@@ -44,37 +51,30 @@ public:
         auto textureCoordsSize = aux::size(_textureCoords);
         auto normalsSize = aux::size(_normals);
 
-        reserveDataBuffer(positionsSize + colorsSize + textureCoordsSize + normalsSize);
+        _engineBackend.reserveDataBufferSize(positionsSize + colorsSize + textureCoordsSize + normalsSize);
         if (positionsSize > 0) {
-            setSubDataBuffer(aux::Index::position, _positions, 0);
+            _engineBackend.setSubDataBuffer(aux::Index::position, _positions, 0);
         }
         if (colorsSize > 0) {
-            setSubDataBuffer(aux::Index::color, _colors, positionsSize);
+            _engineBackend.setSubDataBuffer(aux::Index::color, _colors, positionsSize);
         }
         if (textureCoordsSize > 0) {
-            setSubDataBuffer(aux::Index::texture, _textureCoords, positionsSize + colorsSize);
+            _engineBackend.setSubDataBuffer(aux::Index::texture, _textureCoords, positionsSize + colorsSize);
         }
         if (normalsSize > 0) {
-            setSubDataBuffer(aux::Index::normal, _normals, positionsSize + colorsSize + textureCoordsSize);
+            _engineBackend.setSubDataBuffer(aux::Index::normal, _normals, positionsSize + colorsSize + textureCoordsSize);
         }
     }
 
+    void update() override {}
+
     void render() override {
+        _shader.use();
+        //_shader.setMat4("u_model", _transformation);
         for (auto& texture : _textures) {
             texture.bind();
         }
-
-        getShader().setMat4("u_model", _transformation);
-
-        glBindVertexArray(_VAO);
-        glDrawElements(GL_TRIANGLES, _drawElementCount, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
-
-        glActiveTexture(GL_TEXTURE0);
-    }
-
-    void setShader(Shader& shader) {
-        _shader = shader;
+        _engineBackend.draw();
     }
 
     void setTransformation(glm::mat4 transformation) {
@@ -108,6 +108,10 @@ public:
     }
 
 private:
+    Shader& _shader;
+    ShaderManager _shaderManager{ _shader };
+    OpenGLEngineBackend _engineBackend{ _shaderManager };
+
     std::vector<glm::vec3> _positions;
     std::vector<glm::vec4> _colors;
     std::vector<glm::vec2> _textureCoords;
@@ -115,7 +119,5 @@ private:
     std::vector<Texture> _textures;
     std::vector<unsigned int> _indices;
     glm::mat4 _transformation{ 1.0f };
-
-    unsigned int _drawElementCount{ 0 };
 
 };
