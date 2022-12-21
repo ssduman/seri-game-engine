3df5881d9fb1d488f3fa2cba0a30206f7ffd14ed
ssduman <52033538+ssduman@users.noreply.github.com>
2022-12-21 20:15:49 +0300

    moved input event to header

---
+++ engine/event/InputEvent.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "../control/KeyCode.h"
+#include "../control/KeyAction.h"
+
+#include <memory>
+#include <vector>
+
+namespace events {
+    class IInputEvent {
+    public:
+        IInputEvent() = default;
+
+        virtual ~IInputEvent() = default;
+
+        virtual void onEvent(KeyCode keycode, KeyAction keyAction) = 0;
+
+    };
+
+    template <typename F>
+    class InputEvent : public IInputEvent {
+    public:
+        InputEvent(F f) : _f(std::move(f)) {};
+
+        ~InputEvent() override = default;
+
+        void onEvent(KeyCode keycode, KeyAction keyAction) override {
+            _f(keycode, keyAction);
+        }
+
+    private:
+        F _f;
+
+    };
+
+    template <typename F>
+    inline std::shared_ptr<IInputEvent> makeInputEvent(F f) {
+        return std::make_shared<InputEvent<F>>(std::move(f));
+    }
+}
