b947ca3d4bf810a46ed7f8c81a5371bf307d68be
ssduman <sduman1818@gmail.com>
2022-12-30 11:27:59 +0300

    added glad

---
+++ engine/vendor/glad/premake5.lua
@@ -0,0 +1,28 @@
+project "glad"
+  kind "StaticLib"
+  language "C"
+  staticruntime "off"
+
+  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+  files {
+    "include/glad/gl.h",
+    "include/KHR/khrplatform.h",
+    "src/gl.c"
+  }
+
+  includedirs {
+    "include"
+  }
+
+  filter "system:windows"
+    systemversion "latest"
+
+  filter "configurations:Debug"
+    runtime "Debug"
+    symbols "on"
+
+  filter "configurations:Release"
+    runtime "Release"
+    optimize "on"
