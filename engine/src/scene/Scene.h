30c90e5ed23197a297b922fbcc7c720f9ea9db15
ssduman <52033538+ssduman@users.noreply.github.com>
2023-01-02 15:30:17 +0300

    updated scene logic

--- engine/src/scene/Scene.h
+++ engine/src/scene/Scene.h
@@ -18,48 +18,62 @@ struct Scene {
 class SceneManager {
 public:
     void addScene(std::shared_ptr<Scene> scene) {
-        if (!root) {
-            root = std::move(scene);
+        if (!_root) {
+            LOGGER(verbose, "there is no root, setting this scene with name '" << scene->name << "' as root");
+            _root = std::move(scene);
             return;
         }
 
-        addChild(root, std::move(scene));
+        addChild(_root, std::move(scene));
     }
 
-    void addChild(std::shared_ptr<Scene> scene, std::shared_ptr<Scene> child) {
-        auto pred = [name = child->name](const std::shared_ptr<Scene>& child) {
-            return child->name == name;
-        };
-        auto loc = std::find_if(scene->children.begin(), scene->children.end(), pred);
-        if (loc == scene->children.end()) {
-            child->parent = scene;
-            scene->children.emplace_back(child);
+    void deleteScene(std::shared_ptr<Scene> scene) {
+        if (!_root) {
+            LOGGER(error, "there is no root to delete scene with name '" << scene->name << "' as root");
+            return;
         }
-        else {
-            LOGGER(error, "child with name " << child->name << " already exists");
-        }
+
+        deleteChild(_root, std::move(scene));
     }
 
-    void deleteChild(std::shared_ptr<Scene> scene, std::shared_ptr<Scene> child) {
-        auto pred = [name = child->name](const std::shared_ptr<Scene>& child) {
-            return child->name == name;
-        };
-        auto loc = std::find_if(scene->children.begin(), scene->children.end(), pred);
-        if (loc == scene->children.end()) {
-            child->parent.reset();
-            scene->children.erase(loc);
-        }
-        else {
-            LOGGER(error, "could not find child with name " << child->name);
-        }
+    const std::shared_ptr<Scene>& getRoot() const {
+        return _root;
     }
 
-    std::shared_ptr<Scene> getRoot() {
-        return root;
+    static bool isLeaf(std::shared_ptr<Scene> scene) {
+        return false;
     }
 
 private:
-    std::shared_ptr<Scene> root;
+    void addChild(std::shared_ptr<Scene> scene, std::shared_ptr<Scene> child) {
+        auto pred = [name = child->name](const std::shared_ptr<Scene>& c) {
+            return c->name == name;
+        };
+        auto loc = std::find_if(scene->children.begin(), scene->children.end(), pred);
+        if (loc != scene->children.end()) {
+            LOGGER(error, "child with name " << child->name << " already exists");
+            return;
+        }
+
+        child->parent = scene;
+        scene->children.emplace_back(child);
+    }
+
+    void deleteChild(std::shared_ptr<Scene> scene, std::shared_ptr<Scene> child) {
+        auto pred = [name = child->name](const std::shared_ptr<Scene>& c) {
+            return c->name == name;
+        };
+        auto loc = std::find_if(scene->children.begin(), scene->children.end(), pred);
+        if (loc != scene->children.end()) {
+            LOGGER(error, "could not find child with name " << child->name);
+            return;
+        }
+
+        child->parent.reset();
+        scene->children.erase(loc);
+    }
+
+    std::shared_ptr<Scene> _root;
 
 };
 
