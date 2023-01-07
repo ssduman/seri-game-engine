5d4319ca7f2731c62a2ad89e8f78c7db7952df22
ssduman <52033538+ssduman@users.noreply.github.com>
2023-01-07 17:36:05 +0300

    added more virtual functions to window manager interface

--- engine/src/window/IWindowManager.h
+++ engine/src/window/IWindowManager.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
 struct WindowProperties {
     const char* windowTitle = nullptr;
     bool isFullscreen = false;
@@ -9,7 +12,7 @@ struct WindowProperties {
 
 class IWindowManager {
 public:
-    IWindowManager(WindowProperties windowProperties) : _windowProperties(windowProperties) {};
+    IWindowManager() = default;
 
     virtual ~IWindowManager() = default;
 
@@ -24,6 +27,49 @@ public:
     virtual float updateDeltaTime() = 0;
 
     virtual void* getWindow() = 0;
+
+    virtual  void hideCursor() = 0;
+
+    virtual void enableCursor() = 0;
+
+    virtual  void disableCursor() = 0;
+
+    virtual void setCursorPosMiddle() = 0;
+
+    virtual void setCursorPos(double xpos, double ypos) = 0;
+
+    virtual std::pair<double, double> getCursorPositions() = 0;
+
+    virtual void viewport(int x, int y, int width, int height) = 0;
+
+    virtual int windowShouldClose() = 0;
+
+    virtual void clear() = 0;
+    
+    virtual unsigned int getClearMask() = 0;
+
+    virtual void clearColor(float red = 0.2f, float green = 0.2f, float blue = 0.2f, float alpha = 1.0f) = 0;
+
+    virtual void pollEvents() = 0;
+
+    virtual void swapBuffers() = 0;
+
+    virtual void setWindowShouldCloseToTrue() = 0;
+
+    virtual void setPointSize(float size) = 0;
+
+    virtual void setLineWidth(float width) = 0;
+
+    virtual void setWindowUserPointer(void* pointer) = 0;
+
+    virtual void* getWindowUserPointer() = 0;
+
+    void setWindowProperties(WindowProperties windowProperties) {
+        if (_initialized) {
+            throw std::runtime_error("window is already initialized");
+        }
+        _windowProperties = std::move(windowProperties);
+    }
 
     int getWidth() {
         return _windowProperties.windowWidth;
@@ -47,6 +93,8 @@ public:
 
 protected:
     WindowProperties _windowProperties;
+
+    bool _initialized{ false };
 
     double _lastFrame{ 0.0 };
     double _deltaTime{ 0.0 };
