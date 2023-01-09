2c4b57c6b2deeeff0f8bd820f43af5ec79d111b6
ssduman <52033538+ssduman@users.noreply.github.com>
2023-01-09 10:50:00 +0300

    rearranged include order in seri include

--- engine/src/core/Seri.h
+++ engine/src/core/Seri.h
@@ -1,25 +1,36 @@
 #pragma once
 
 #include "../util/Util.h"
+
 #include "../core/State.h"
 #include "../core/Object.h"
 #include "../core/Entity.h"
+#include "../core/IRunner.h"
 #include "../core/AuxiliaryStructs.h"
 #include "../core/AuxiliaryStructsBuilder.h"
-#include "../core/IRunner.h"
+
 #include "../scene/Layer.h"
 #include "../scene/Scene.h"
 #include "../scene/SceneBuilder.h"
 #include "../scene/SceneVisitor.h"
 #include "../scene/SceneIterator.h"
+
 #include "../model/Model.h"
+
 #include "../camera/ICamera.h"
+
 #include "../texture/Color.h"
 #include "../texture/Typer.h"
 #include "../texture/Skybox.h"
+
 #include "../logging/Logger.h"
+
 #include "../control/IControl.h"
+
+#include "../event/IEvent.h"
 #include "../event/EventManager.h"
+
 #include "../window/IWindowManager.h"
 #include "../window/WindowManagerFactory.h"
+
 #include "../renderer/OpenGLEngineBackend.h"
