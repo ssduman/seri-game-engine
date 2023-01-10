97fbb8184750df7b1b92f8fdaee97f65217b99b9
ssduman <52033538+ssduman@users.noreply.github.com>
2023-01-11 00:08:54 +0300

    added event data for event callbacks

---
+++ engine/src/event/EventData.h
@@ -0,0 +1,235 @@
+#pragma once
+
+#include "KeyEvent.h"
+#include "EventType.h"
+#include "WindowEvent.h"
+#include "MouseButtonEvent.h"
+
+#include <vector>
+#include <string>
+#include <sstream>
+
+// interface
+struct IEventData {
+    IEventData(EventType type_) : type(type_) {}
+
+    virtual ~IEventData() {}
+
+    virtual std::string toString() = 0;
+
+    EventType type = EventType::unknown;
+};
+
+// input
+struct KeyEventData : public IEventData {
+    KeyEventData(KeyCode key_, int scancode_, InputAction action_, std::vector<InputModifier> mods_)
+        : IEventData(EventType::key), key(key_), scancode(scancode_), action(action_), mods(std::move(mods_)) {}
+
+    ~KeyEventData() override = default;
+
+    std::string toString() override {
+        std::stringstream modss;
+        for (auto mod : mods) {
+            modss << "|" << ::toString(mod) << "|";
+        }
+
+        std::stringstream ss;
+        ss << ::toString(type) << ": {"
+            << "'key': " << ::toString(key) << ", "
+            << "'scancode': " << scancode << ", "
+            << "'action': " << ::toString(action) << ", "
+            << "'mods': " << modss.str()
+            << "}";
+
+        return ss.str();
+    }
+
+    KeyCode key;
+    int scancode;
+    InputAction action;
+    std::vector<InputModifier> mods;
+};
+
+struct CharacterEventData : public IEventData {
+    CharacterEventData(unsigned int codepoint_)
+        : IEventData(EventType::character), codepoint(codepoint_) {}
+
+    ~CharacterEventData() override = default;
+
+    std::string toString() override {
+        std::stringstream ss;
+        ss << ::toString(type) << ": {"
+            << "'codepoint': " << codepoint
+            << "}";
+
+        return ss.str();
+    }
+
+    unsigned int codepoint;
+};
+
+struct CharacterModsEventData : public IEventData {
+    CharacterModsEventData(unsigned int codepoint_, std::vector<InputModifier> mods_)
+        : IEventData(EventType::character_mods), codepoint(codepoint_), mods(std::move(mods_)) {}
+
+    ~CharacterModsEventData() override = default;
+
+    std::string toString() override {
+        std::stringstream modss;
+        for (auto mod : mods) {
+            modss << "|" << ::toString(mod) << "|";
+        }
+
+        std::stringstream ss;
+        ss << ::toString(type) << ": {"
+            << "'codepoint': " << codepoint << ", "
+            << "'mods': " << modss.str()
+            << "}";
+
+        return ss.str();
+    }
+
+    unsigned int codepoint;
+    std::vector<InputModifier> mods;
+};
+
+// mouse
+struct MouseEnterEventData : public IEventData {
+    MouseEnterEventData(bool entered_)
+        : IEventData(EventType::mouse_enter), entered(entered_) {}
+
+    ~MouseEnterEventData() override = default;
+
+    std::string toString() override {
+        std::stringstream ss;
+        ss << ::toString(type) << ": {"
+            << "'entered': " << (entered ? "true" : "false")
+            << "}";
+
+        return ss.str();
+    }
+
+    bool entered;
+};
+
+struct MouseButtonEventData : public IEventData {
+    MouseButtonEventData(MouseButtonCode button_, InputAction action_, InputModifier mods_)
+        : IEventData(EventType::mouse_button), button(button_), action(action_), mods(mods_) {}
+
+    ~MouseButtonEventData() override = default;
+
+    std::string toString() override {
+        std::stringstream ss;
+        ss << ::toString(type) << ": {"
+            << "'button': " << ::toString(button) << ", "
+            << "'action': " << ::toString(action) << ", "
+            << "'mods': " << ::toString(mods)
+            << "}";
+
+        return ss.str();
+    }
+
+    MouseButtonCode button;
+    InputAction action;
+    InputModifier mods;
+};
+
+struct MouseScrollEventData : public IEventData {
+    MouseScrollEventData(double xoffset_, double yoffset_)
+        : IEventData(EventType::mouse_scroll), xoffset(xoffset_), yoffset(yoffset_) {}
+
+    ~MouseScrollEventData() override = default;
+
+    std::string toString() override {
+        std::stringstream ss;
+        ss << ::toString(type) << ": {"
+            << "'xoffset': " << xoffset << ", "
+            << "'yoffset': " << yoffset
+            << "}";
+
+        return ss.str();
+    }
+
+    double xoffset;
+    double yoffset;
+};
+
+struct MousePositionEventData : public IEventData {
+    MousePositionEventData(double xpos_, double ypos_)
+        : IEventData(EventType::mouse_position), xpos(xpos_), ypos(ypos_) {}
+
+    ~MousePositionEventData() override = default;
+
+    std::string toString() override {
+        std::stringstream ss;
+        ss << ::toString(type) << ": {"
+            << "'xpos': " << xpos << ", "
+            << "'ypos': " << ypos
+            << "}";
+
+        return ss.str();
+    }
+
+    double xpos;
+    double ypos;
+};
+
+// window
+struct WindowDropEventData : public IEventData {
+    WindowDropEventData(std::vector<std::string> paths_)
+        : IEventData(EventType::window_drop), paths(std::move(paths_)) {}
+
+    ~WindowDropEventData() override = default;
+
+    std::string toString() override {
+        std::stringstream pathss;
+        for (auto& path : paths) {
+            pathss << "|" << path << "|";
+        }
+
+        std::stringstream ss;
+        ss << ::toString(type) << ": {"
+            << "'paths': " << pathss.str()
+            << "}";
+
+        return ss.str();
+    }
+
+    std::vector<std::string> paths;
+};
+
+struct WindowCloseEventData : public IEventData {
+    WindowCloseEventData()
+        : IEventData(EventType::window_close) {}
+
+    ~WindowCloseEventData() override = default;
+
+    std::string toString() override {
+        std::stringstream ss;
+        ss << ::toString(type) << ": {"
+            << "'window_close'"
+            << "}";
+
+        return ss.str();
+    }
+};
+
+struct WindowResizeEventData : public IEventData {
+    WindowResizeEventData(int width_, int height_)
+        : IEventData(EventType::window_resize), width(width_), height(height_) {}
+
+    ~WindowResizeEventData() override = default;
+
+    std::string toString() override {
+        std::stringstream ss;
+        ss << ::toString(type) << ": {"
+            << "'width': " << width << ", "
+            << "'height': " << height
+            << "}";
+
+        return ss.str();
+    }
+
+    double width;
+    double height;
+};
