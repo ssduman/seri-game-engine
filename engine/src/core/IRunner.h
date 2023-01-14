e55e52fe304cf2b4448822f374e14930b94d8a22
ssduman <52033538+ssduman@users.noreply.github.com>
2023-01-14 19:50:46 +0300

    deleted unused includes

--- engine/src/core/IRunner.h
+++ engine/src/core/IRunner.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#include "State.h"
-#include "../util/Util.h"
-#include "../scene/Layer.h"
-#include "../logging/Logger.h"
-#include "../window/IWindowManager.h"
-
 class IRunner {
 public:
     IRunner() = default;
