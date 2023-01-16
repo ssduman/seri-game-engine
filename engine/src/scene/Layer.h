09e04be577adbf44a004e82a0f361d0c9b26d4d6
ssduman <52033538+ssduman@users.noreply.github.com>
2023-01-16 21:35:45 +0300

    updated layer

--- engine/src/scene/Layer.h
+++ engine/src/scene/Layer.h
@@ -1,28 +1,74 @@
 #pragma once
 
-#include "../core/Object.h"
-
+#include <map>
+#include <memory>
+#include <string>
 #include <vector>
 
-class Layer {
-public:
-    Layer() = default;
+struct Layerable {
+    Layerable(std::string layerName_) : layerName(layerName_) {}
 
-    void addLayer(std::shared_ptr<Object> entity) {
-        _layers.emplace_back(entity);
+    virtual ~Layerable() = default;
+
+    virtual void draw() = 0;
+
+    std::string layerName;
+};
+
+class LayerManager {
+public:
+    void addGuiLayer(std::shared_ptr<Layerable> layer) {
+        _guiLayer = layer;
     }
 
-    void deleteLayer() {
-        if (!_layers.empty()) {
-            _layers.pop_back();
+    void deleteGuiLayer(std::shared_ptr<Layerable> layer) {
+        _guiLayer.reset();
+    }
+
+    void addDebugLayer(std::shared_ptr<Layerable> layer) {
+        _debugLayer = layer;
+    }
+
+    void deleteDebugLayer(std::shared_ptr<Layerable> layer) {
+        _debugLayer.reset();
+    }
+
+    void addUserLayer(std::shared_ptr<Layerable> layer) {
+        _userLayer = layer;
+    }
+
+    void deleteUserLayer(std::shared_ptr<Layerable> layer) {
+        _userLayer.reset();
+    }
+
+    void addSceneLayer(std::shared_ptr<Layerable> layer) {
+        _sceneLayer = layer;
+    }
+
+    void deleteSceneLayer(std::shared_ptr<Layerable> layer) {
+        _sceneLayer.reset();
+    }
+
+    void draw() {
+        if (_userLayer) {
+            _userLayer->draw();
+        }
+        if (_sceneLayer) {
+            _sceneLayer->draw();
+        }
+        if (_debugLayer) {
+            _debugLayer->draw();
+        }
+        if (_guiLayer) {
+            _guiLayer->draw();
         }
     }
 
-    std::vector<std::shared_ptr<Object>> getLayers() {
-        return _layers;
-    }
-
 private:
-    std::vector<std::shared_ptr<Object>> _layers{};
+
+    std::shared_ptr<Layerable> _guiLayer;
+    std::shared_ptr<Layerable> _userLayer;
+    std::shared_ptr<Layerable> _debugLayer;
+    std::shared_ptr<Layerable> _sceneLayer;
 
 };
