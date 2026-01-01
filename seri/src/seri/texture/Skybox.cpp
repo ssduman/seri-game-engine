b9a72a66de1e22e10e77ad678fe3a1b6531725ca
ssduman <sduman1818@gmail.com>
2026-01-01 12:14:57 +0300

    refactored skybox

---
+++ seri/src/seri/texture/Skybox.cpp
@@ -0,0 +1,155 @@
+#include "Seripch.h"
+
+#include "seri/texture/Skybox.h"
+
+namespace seri
+{
+	Skybox::Skybox()
+	{
+		_faces = {
+			"assets/textures/skybox/1/right.jpg",
+			"assets/textures/skybox/1/left.jpg",
+			"assets/textures/skybox/1/top.jpg",
+			"assets/textures/skybox/1/bottom.jpg",
+			"assets/textures/skybox/1/front.jpg",
+			"assets/textures/skybox/1/back.jpg",
+		};
+
+		_faces = {
+			"assets/textures/skybox/2/px.jpg",
+			"assets/textures/skybox/2/nx.jpg",
+			"assets/textures/skybox/2/py.jpg",
+			"assets/textures/skybox/2/ny.jpg",
+			"assets/textures/skybox/2/pz.jpg",
+			"assets/textures/skybox/2/nz.jpg",
+		};
+
+		_faces = {
+			"assets/textures/skybox/3/px.jpg",
+			"assets/textures/skybox/3/nx.jpg",
+			"assets/textures/skybox/3/py.jpg",
+			"assets/textures/skybox/3/ny.jpg",
+			"assets/textures/skybox/3/pz.jpg",
+			"assets/textures/skybox/3/nz.jpg",
+		};
+
+		Init();
+	}
+
+	Skybox::Skybox(std::vector<std::string> faces) : _faces(std::move(faces))
+	{
+		Init();
+	}
+
+	void Skybox::Init()
+	{
+		_texture = TextureBase::Create();
+		_material = std::make_shared<Material>();
+		_material->shader = ShaderLibrary::Find("skybox");;
+		_material->SetTexture("u_skybox", _texture);
+
+		SetDefaultPositions();
+		LoadCubemap();
+	}
+
+	void Skybox::Update()
+	{
+		seri::RenderingManager::Begin(seri::Graphic::GetCameraPerspective());
+
+		_material->SetMat4("u_view_skybox", glm::mat4(glm::mat3(seri::Graphic::GetCameraPerspective()->GetView())));
+
+		seri::RenderCommand renderCommand_skybox{};
+		renderCommand_skybox.name = "skybox";
+		renderCommand_skybox.desc.depthFunc = DepthFunc::l_equal;
+		renderCommand_skybox.camera = seri::Graphic::GetCameraPerspective();
+		renderCommand_skybox.drawMode = DrawMode::arrays;
+		renderCommand_skybox.material = _material;
+		renderCommand_skybox.vao = _vertexArray;
+		renderCommand_skybox.rt = seri::RenderingManager::GetEditorRT();
+		renderCommand_skybox.count = static_cast<uint32_t>(_positions.size());
+		seri::RenderingManager::Submit(renderCommand_skybox);
+
+		seri::RenderCommand renderCommand_restore{};
+		renderCommand_restore.name = "skybox_restore";
+		renderCommand_restore.noop = true;
+		seri::RenderingManager::Submit(renderCommand_restore);
+
+		seri::RenderingManager::End();
+	}
+
+	void Skybox::SetDefaultPositions()
+	{
+		_positions = {
+			{ -1.0f, 1.0f, -1.0f },
+			{ -1.0f, -1.0f, -1.0f },
+			{ 1.0f, -1.0f, -1.0f },
+			{ 1.0f, -1.0f, -1.0f },
+			{ 1.0f, 1.0f, -1.0f },
+			{ -1.0f, 1.0f, -1.0f },
+
+			{ -1.0f, -1.0f, 1.0f },
+			{ -1.0f, -1.0f, -1.0f },
+			{ -1.0f, 1.0f, -1.0f },
+			{ -1.0f, 1.0f, -1.0f },
+			{ -1.0f, 1.0f, 1.0f },
+			{ -1.0f, -1.0f, 1.0f },
+
+			{ 1.0f, -1.0f, -1.0f },
+			{ 1.0f, -1.0f, 1.0f },
+			{ 1.0f, 1.0f, 1.0f },
+			{ 1.0f, 1.0f, 1.0f },
+			{ 1.0f, 1.0f, -1.0f },
+			{ 1.0f, -1.0f, -1.0f },
+
+			{ -1.0f, -1.0f, 1.0f },
+			{ -1.0f, 1.0f, 1.0f },
+			{ 1.0f, 1.0f, 1.0f },
+			{ 1.0f, 1.0f, 1.0f },
+			{ 1.0f, -1.0f, 1.0f },
+			{ -1.0f, -1.0f, 1.0f },
+
+			{ -1.0f, 1.0f, -1.0f },
+			{ 1.0f, 1.0f, -1.0f },
+			{ 1.0f, 1.0f, 1.0f },
+			{ 1.0f, 1.0f, 1.0f },
+			{ -1.0f, 1.0f, 1.0f },
+			{ -1.0f, 1.0f, -1.0f },
+
+			{ -1.0f, -1.0f, -1.0f },
+			{ -1.0f, -1.0f, 1.0f },
+			{ 1.0f, -1.0f, -1.0f },
+			{ 1.0f, -1.0f, -1.0f },
+			{ -1.0f, -1.0f, 1.0f },
+			{ 1.0f, -1.0f, 1.0f },
+		};
+
+		auto vertexBuffer = seri::VertexBufferBase::Create(&_positions[0], _positions.size() * 3 * sizeof(float));
+		vertexBuffer->AddElement(
+			{ seri::LayoutLocation::vertex, seri::ShaderDataType::float3_type, false }
+		);
+
+		_vertexArray = seri::VertexArrayBase::Create();
+		_vertexArray->AddVertexBuffer(vertexBuffer);
+	}
+
+	void Skybox::LoadCubemap(bool flip)
+	{
+		if (_faces.size() != 6)
+		{
+			LOGGER(error, "[skybox] there should be exactly 6 textures for skybox");
+			return;
+		}
+
+		TextureDesc desc{};
+		desc.flip = flip;
+		desc.target = TextureTarget::cube_map;
+		desc.wrapS = TextureWrap::clamp_to_edge;
+		desc.wrapT = TextureWrap::clamp_to_edge;
+		desc.wrapR = TextureWrap::clamp_to_edge;
+		desc.magFilter = TextureMagFilter::linear;
+		desc.minFilter = TextureMinFilter::linear;
+		_texture->Init(desc);
+		_texture->LoadCubeMap(_faces);
+	}
+
+}
--- seri/src/seri/texture/Skybox.h
+++ seri/src/seri/texture/Skybox.h
@@ -13,141 +13,15 @@ namespace seri
 	class Skybox
 	{
 	public:
-		Skybox()
-		{
-			_faces = {
-				"assets/textures/skybox/right.jpg",
-				"assets/textures/skybox/left.jpg",
-				"assets/textures/skybox/bottom.jpg",
-				"assets/textures/skybox/top.jpg",
-				"assets/textures/skybox/front.jpg",
-				"assets/textures/skybox/back.jpg",
-			};
+		Skybox();
+		Skybox(std::vector<std::string> faces);
 
-			Init();
-		}
-
-		Skybox(std::vector<std::string> faces) : _faces(std::move(faces))
-		{
-			Init();
-		}
-
-		void Init()
-		{
-			_texture = TextureBase::Create();
-			_material = std::make_shared<Material>();
-			_material->shader = ShaderLibrary::Find("skybox");;
-			_material->SetTexture("u_skybox", _texture);
-
-			SetDefaultPositions();
-			LoadCubemap();
-		}
-
-		void Update()
-		{
-			seri::RenderingManager::Begin(seri::Graphic::GetCameraPerspective());
-
-			_material->SetMat4("u_view_skybox", glm::mat4(glm::mat3(seri::Graphic::GetCameraPerspective()->GetView())));
-
-			seri::RenderCommand renderCommand_skybox{};
-			renderCommand_skybox.name = "skybox";
-			renderCommand_skybox.desc.depthFunc = DepthFunc::l_equal;
-			renderCommand_skybox.camera = seri::Graphic::GetCameraPerspective();
-			renderCommand_skybox.drawMode = DrawMode::arrays;
-			renderCommand_skybox.material = _material;
-			renderCommand_skybox.vao = _vertexArray;
-			renderCommand_skybox.rt = seri::RenderingManager::GetEditorRT();
-			renderCommand_skybox.count = static_cast<uint32_t>(_positions.size());
-			seri::RenderingManager::Submit(renderCommand_skybox);
-
-			seri::RenderCommand renderCommand_restore{};
-			renderCommand_restore.name = "skybox_restore";
-			renderCommand_restore.noop = true;
-			seri::RenderingManager::Submit(renderCommand_restore);
-
-			seri::RenderingManager::End();
-		}
+		void Init();
+		void Update();
 
 	private:
-		void SetDefaultPositions()
-		{
-			_positions = {
-				{ -1.0f, 1.0f, -1.0f },
-				{ -1.0f, -1.0f, -1.0f },
-				{ 1.0f, -1.0f, -1.0f },
-				{ 1.0f, -1.0f, -1.0f },
-				{ 1.0f, 1.0f, -1.0f },
-				{ -1.0f, 1.0f, -1.0f },
-
-				{ -1.0f, -1.0f, 1.0f },
-				{ -1.0f, -1.0f, -1.0f },
-				{ -1.0f, 1.0f, -1.0f },
-				{ -1.0f, 1.0f, -1.0f },
-				{ -1.0f, 1.0f, 1.0f },
-				{ -1.0f, -1.0f, 1.0f },
-
-				{ 1.0f, -1.0f, -1.0f },
-				{ 1.0f, -1.0f, 1.0f },
-				{ 1.0f, 1.0f, 1.0f },
-				{ 1.0f, 1.0f, 1.0f },
-				{ 1.0f, 1.0f, -1.0f },
-				{ 1.0f, -1.0f, -1.0f },
-
-				{ -1.0f, -1.0f, 1.0f },
-				{ -1.0f, 1.0f, 1.0f },
-				{ 1.0f, 1.0f, 1.0f },
-				{ 1.0f, 1.0f, 1.0f },
-				{ 1.0f, -1.0f, 1.0f },
-				{ -1.0f, -1.0f, 1.0f },
-
-				{ -1.0f, 1.0f, -1.0f },
-				{ 1.0f, 1.0f, -1.0f },
-				{ 1.0f, 1.0f, 1.0f },
-				{ 1.0f, 1.0f, 1.0f },
-				{ -1.0f, 1.0f, 1.0f },
-				{ -1.0f, 1.0f, -1.0f },
-
-				{ -1.0f, -1.0f, -1.0f },
-				{ -1.0f, -1.0f, 1.0f },
-				{ 1.0f, -1.0f, -1.0f },
-				{ 1.0f, -1.0f, -1.0f },
-				{ -1.0f, -1.0f, 1.0f },
-				{ 1.0f, -1.0f, 1.0f },
-			};
-
-			auto vertexBuffer = seri::VertexBufferBase::Create(&_positions[0], _positions.size() * 3 * sizeof(float));
-			vertexBuffer->AddElement(
-				{ seri::LayoutLocation::vertex, seri::ShaderDataType::float3_type, false }
-			);
-
-			_vertexArray = seri::VertexArrayBase::Create();
-			_vertexArray->AddVertexBuffer(vertexBuffer);
-		}
-
-		void LoadCubemap(bool flip = false)
-		{
-			if (_faces.empty() || _faces.size() != 6)
-			{
-				LOGGER(error, "there should be exactly 6 textures for skybox");
-				return;
-			}
-
-			if (!flip)
-			{
-				std::swap(_faces[2], _faces[3]);
-			}
-
-			TextureDesc desc{};
-			desc.flip = flip;
-			desc.target = TextureTarget::cube_map;
-			desc.wrapS = TextureWrap::clamp_to_edge;
-			desc.wrapT = TextureWrap::clamp_to_edge;
-			desc.wrapR = TextureWrap::clamp_to_edge;
-			desc.magFilter = TextureMagFilter::linear;
-			desc.minFilter = TextureMinFilter::linear;
-			_texture->Init(desc);
-			_texture->LoadCubeMap(_faces);
-		}
+		void SetDefaultPositions();
+		void LoadCubemap(bool flip = false);
 
 		std::vector<std::string> _faces;
 		std::vector<glm::vec3> _positions;
