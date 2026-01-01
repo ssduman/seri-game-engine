257ea9e4db49ddb87c3ba11f5dcf366191a91031
ssduman <sduman1818@gmail.com>
2026-01-01 13:16:34 +0300

    refactored shader library

---
+++ seri/src/seri/shader/ShaderLibrary.cpp
@@ -0,0 +1,79 @@
+#include "Seripch.h"
+
+#include "seri/shader/ShaderLibrary.h"
+
+namespace seri
+{
+	ShaderLibrary& ShaderLibrary::GetInstance()
+	{
+		static ShaderLibrary instance;
+		return instance;
+	}
+
+	void ShaderLibrary::Init(const char* shaderFolderPath)
+	{
+		GetInstance()._predefinedShaders = {};
+
+		for (const auto& entry : std::filesystem::directory_iterator(shaderFolderPath))
+		{
+			if (Util::Contains(entry.path().string(), ".smeta"))
+			{
+				continue;
+			}
+
+			std::string name = entry.path().stem().string();
+			std::string text = Util::ReadFileAtPath(entry.path().string().c_str());
+
+			ShaderInfo info;
+			info.valid = true;
+			info.name = name;
+			info.vsCode = Util::GetContentOfToken(text, "#beg_vs", "#end_vs");
+			info.fsCode = Util::GetContentOfToken(text, "#beg_fs", "#end_fs");
+			GetInstance()._predefinedShaders[name] = info;
+		}
+
+		LOGGER(info, fmt::format("[shader library] init done, count: {}", GetInstance()._predefinedShaders.size()));
+	}
+
+	ShaderLibrary::ShaderInfo& ShaderLibrary::Get(const std::string& name)
+	{
+		if (GetInstance()._predefinedShaders.find(name) != GetInstance()._predefinedShaders.end())
+		{
+			return GetInstance()._predefinedShaders[name];
+		}
+
+		throw std::runtime_error("[shader library] not found: " + name);
+	}
+
+	std::shared_ptr<ShaderBase> ShaderLibrary::Find(const std::string& name)
+	{
+		auto& predef = GetInstance().Get(name);
+		auto shader = ShaderBase::Create();
+		shader->Init(predef.vsCode.c_str(), predef.fsCode.c_str());
+		return shader;
+	}
+
+	void ShaderLibrary::SetView(std::shared_ptr<ShaderBase>& shader, const glm::mat4& view)
+	{
+		shader->Bind();
+		shader->SetMat4("u_view", view);
+		shader->Unbind();
+	}
+
+	void ShaderLibrary::SetProjection(std::shared_ptr<ShaderBase>& shader, const glm::mat4& projection)
+	{
+		shader->Bind();
+		shader->SetMat4("u_projection", projection);
+		shader->Unbind();
+	}
+
+	void ShaderLibrary::SetMVP(std::shared_ptr<ShaderBase>& shader, const std::shared_ptr<CameraBase>& camera)
+	{
+		shader->Bind();
+		shader->SetMat4("u_model", glm::mat4{ 1.0f });
+		shader->SetMat4("u_view", camera->GetView());
+		shader->SetMat4("u_projection", camera->GetProjection());
+		shader->Unbind();
+	}
+
+}
--- seri/src/seri/shader/ShaderLibrary.h
+++ seri/src/seri/shader/ShaderLibrary.h
@@ -23,136 +23,21 @@ namespace seri
 		};
 
 		ShaderLibrary(ShaderLibrary const&) = delete;
-
 		void operator=(ShaderLibrary const&) = delete;
 
-		static ShaderLibrary& GetInstance()
-		{
-			static ShaderLibrary instance;
-			return instance;
-		}
+		static ShaderLibrary& GetInstance();
+		static void Init(const char* shaderFolderPath);
 
-		static void Init(const char* shaderFolderPath)
-		{
-			GetInstance()._predefinedShaders = {};
+		static ShaderInfo& Get(const std::string& name);
+		static std::shared_ptr<ShaderBase> Find(const std::string& name);
 
-			for (const auto& entry : std::filesystem::directory_iterator(shaderFolderPath))
-			{
-				if (Util::Contains(entry.path().string(), ".smeta"))
-				{
-					continue;
-				}
-
-				std::string name = entry.path().stem().string();
-				std::string text = Util::ReadFileAtPath(entry.path().string().c_str());
-
-				ShaderInfo info;
-				info.valid = true;
-				info.name = name;
-				info.vsCode = Util::GetContentOfToken(text, "#beg_vs", "#end_vs");
-				info.fsCode = Util::GetContentOfToken(text, "#beg_fs", "#end_fs");
-				GetInstance()._predefinedShaders[name] = info;
-			}
-
-			LOGGER(info, fmt::format("[shader library] init done, count: {}", GetInstance()._predefinedShaders.size()));
-		}
-
-		static ShaderInfo& Get(const std::string& name)
-		{
-			if (GetInstance()._predefinedShaders.find(name) != GetInstance()._predefinedShaders.end())
-			{
-				return GetInstance()._predefinedShaders[name];
-			}
-
-			throw std::runtime_error("[shader library] not found: " + name);
-		}
-
-		static std::shared_ptr<ShaderBase> Find(const std::string& name)
-		{
-			auto& predef = GetInstance().Get(name);
-			auto shader = ShaderBase::Create();
-			shader->Init(predef.vsCode.c_str(), predef.fsCode.c_str());
-			return shader;
-		}
-
-		static void SetView(std::shared_ptr<ShaderBase>& shader, const glm::mat4& view)
-		{
-			shader->Bind();
-			shader->SetMat4("u_view", view);
-			shader->Unbind();
-		}
-
-		static void SetModel(std::shared_ptr<ShaderBase>& shader, const glm::mat4& model)
-		{
-			shader->Bind();
-			shader->SetMat4("u_model", model);
-			shader->Unbind();
-		}
-
-		static void SetProjection(std::shared_ptr<ShaderBase>& shader, const glm::mat4& projection)
-		{
-			shader->Bind();
-			shader->SetMat4("u_projection", projection);
-			shader->Unbind();
-		}
-
-		static void SetVP(std::shared_ptr<ShaderBase>& shader, const std::shared_ptr<CameraBase>& camera)
-		{
-			shader->Bind();
-			shader->SetMat4("u_view", camera->GetView());
-			shader->SetMat4("u_projection", camera->GetProjection());
-			shader->Unbind();
-		}
-
-		static void SetMVP(std::shared_ptr<ShaderBase>& shader, const std::shared_ptr<CameraBase>& camera)
-		{
-			shader->Bind();
-			shader->SetMat4("u_model", glm::mat4{ 1.0f });
-			shader->SetMat4("u_view", camera->GetView());
-			shader->SetMat4("u_projection", camera->GetProjection());
-			shader->Unbind();
-		}
-
-		static void SetTRS(std::shared_ptr<ShaderBase>& shader, const std::shared_ptr<CameraBase>& camera)
-		{
-			shader->Bind();
-			shader->SetMat4("u_model", glm::mat4{ 1.0f });
-			shader->SetMat4("u_view", camera->GetView());
-			shader->SetMat4("u_projection", camera->GetProjection());
-			shader->Unbind();
-		}
-
-		static void SetPosition(std::shared_ptr<ShaderBase>& shader, const glm::vec2& position)
-		{
-			shader->Bind();
-			shader->SetFloat2("u_position", position);
-			shader->Unbind();
-		}
-
-		static void SetPosition(std::shared_ptr<ShaderBase>& shader, const glm::vec3& position)
-		{
-			shader->Bind();
-			shader->SetFloat3("u_position", position);
-			shader->Unbind();
-		}
-
-		static void SetColor(std::shared_ptr<ShaderBase>& shader, const glm::vec4& color)
-		{
-			shader->Bind();
-			shader->SetFloat4("u_color", color);
-			shader->Unbind();
-		}
+		static void SetView(std::shared_ptr<ShaderBase>& shader, const glm::mat4& view);
+		static void SetProjection(std::shared_ptr<ShaderBase>& shader, const glm::mat4& projection);
+		static void SetMVP(std::shared_ptr<ShaderBase>& shader, const std::shared_ptr<CameraBase>& camera);
 
 	private:
-		ShaderLibrary()
-		{
-			//LOGGER(info, "[shader] manager init");
-		}
-
-		~ShaderLibrary()
-		{
-			//LOGGER(info, "[shader] manager release");
-		}
+		ShaderLibrary() = default;
+		~ShaderLibrary() = default;
 
 		std::unordered_map<std::string, ShaderInfo> _predefinedShaders;
 
